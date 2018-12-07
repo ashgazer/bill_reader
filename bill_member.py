2730651c966e8e6a14c07bc5fe53c6d841eba8b0
Ashik Pirmohamed <ashik.pirmohamed@wahanda.com>
2018-12-07 22:11:57 +0000

    mess around

--- bill_member.py
+++ bill_member.py
@@ -1,6 +1,46 @@
+from load_readings import get_readings
+from dateutil.parser import parse
+import pytz
+from tariff import BULB_TARIFF
 
 
-def calculate_bill(member_id=None, account_id=None, bill_date=None):
+
+def calc_ashik(member_id, account_id,bill_date=None):
+    utc = pytz.UTC
+    readings = get_readings()
+    readings = readings[member_id]
+    bill_date = parse(bill_date)
+    bill_date = utc.localize(bill_date)
+
+    latest_reading =[]
+
+
+    for accounts in readings:
+        if account_id in accounts:
+            for util_type in accounts[account_id]:
+                if 'electricity' in util_type:
+                    for reading in util_type['electricity']:
+                        if parse(reading['readingDate']) <= bill_date:
+                            latest_reading.append(reading)
+
+                    latest_reading = latest_reading[len(latest_reading)-2:]
+
+                    kwh = latest_reading[1]['cumulative'] - latest_reading[0]['cumulative']
+                    SC = BULB_TARIFF['electricity']['standing_charge'] * float(bill_date.strftime('%d'))
+                    units = kwh * BULB_TARIFF['electricity']['unit_rate']
+                    print((SC+units)/100)
+
+
+
+
+
+
+
+
+
+calc_ashik('member-123','account-abc', '2017-08-31')
+#
+def calculate_bill(member_id, account_id, bill_date):
     # TODO REFACTOR ME :)
     if (member_id == 'member-123' and
         account_id == 'ALL' and
@@ -10,6 +50,8 @@ def calculate_bill(member_id=None, account_id=None, bill_date=None):
     else:
         amount = 0.
         kwh = 0
+
+
     return amount, kwh
 
 
