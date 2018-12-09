f0bdec69b15c0c1c9ccf9ae32307fc0d38570e72
Ashik Pirmohamed <ashik.pirmohamed@wahanda.com>
2018-12-09 10:56:11 +0000

    add test from extract days function

--- test_bill_member.py
+++ test_bill_member.py
@@ -1,7 +1,7 @@
 import datetime
 import unittest
 
-from bill_member import calculate_bill
+from bill_member import calculate_bill, extract_days
 
 
 class TestBillMember(unittest.TestCase):
@@ -13,6 +13,11 @@ class TestBillMember(unittest.TestCase):
         self.assertEqual(amount, 27.57)
         self.assertEqual(kwh, 167)
 
+
+    def test_extract_days(self):
+        date_to_test = datetime.datetime(2017, 8, 31, 0, 0)
+        days = extract_days(date_to_test)
+        self.assertAlmostEquals(days,31)
 
 if __name__ == '__main__':
     unittest.main()
